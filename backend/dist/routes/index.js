2e55278ad20ed50d8efc25df64d3872e8140dd80
Tuguldur0107 <tuguldur.enkhtuvshin@yahoo.com>
2025-08-06 02:37:03 +0800

    build: generate dist
    build: generate dist

---
+++ backend/dist/routes/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+router.get('/', (req, res) => {
+    res.send('X-SENDER backend working!');
+});
+exports.default = router;
